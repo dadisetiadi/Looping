5b0bf03a30ef2fb1e309ef3fb81d126e8d4f5e9d
Dadi <70506176+dadisetiadi@users.noreply.github.com>
2020-11-09 23:14:35 +0700

    Add files via upload
    C#

---
+++ Looping/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int month;
+            month = 3;
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("January");
+                    break;
+                case 2:
+                    Console.WriteLine("February");
+                    break;
+                case 3:
+                    Console.WriteLine("March");
+                    break;
+                default:
+                    Console.WriteLine("Error: Invalid Month");
+                    break;
+            }
+            Console.Read();
+            */
+
+
+            int a;
+            a = 1;
+            switch (a + 1)
+            {
+               
+                    case 1:
+                    Console.WriteLine("nilai a adalah nol");
+                    break;
+
+                    case 2:
+                    Console.WriteLine("nilai a adalah satu");
+                    break;
+                    case 3:
+                    Console.WriteLine("nilai a adalah dua");
+                    break;
+
+                    default:
+                    a = -1;
+                    Console.WriteLine("nilai a adalah min satu");
+                    break;
+            }
+            Console.Read();
+
+        }
+    }
+}
---
+++ Looping/perulangan/perulangan/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace perulangan
+{
+    class Program
+    {
+        /*
+        static void Main(string[] args)
+        {
+            int count;
+            for (count = 1; count <= 10; count+=1)
+            {
+                Console.WriteLine(count+" hasilnya adalah " + count*count );
+            }
+            Console.Read();
+         */
+
+
+        /*
+         
+        static void Main(string[] args)
+        {
+            int count;
+            for (count = 10; count <= 200; count++)
+            {
+                Console.WriteLine( count + " mph = "  + count*1.609  + " kph" );
+            }
+            Console.Read();
+         */
+
+
+        static void Main(string[] args)
+        {
+            int count, a, hasil;
+            hasil = 1;
+            Console.Write("Masukan angka : ");
+            a = int.Parse(Console.ReadLine());
+
+            for (count = 1; count <= a; count++)
+            {
+                hasil = hasil * count;
+                
+                Console.WriteLine(a + "! =" + hasil);
+            }
+            Console.Read();
+
+        }
+    }
+}
